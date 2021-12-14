9f4a521c6433908f977ede0f4d3e5c4a401e7c0b
pramod <kpramod8095@allstate.com>
2021-12-14 13:02:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import React, {useEffect, useState} from 'react'
+
+
+
+
+async function getResponce(){
+
+  return axios.get('https://randomuser.me/api')
+  .then(({data}) =>{
+    // handle success
+    console.log(data);
+    return data
+  })
+  .catch((error) =>{
+    // handle error
+    console.log(error);
+  })
+}
+
+function getFullUserName(userInfo){
+  const { name:{title, first, last}} = userInfo;
+  return `${title}. ${first} ${last}`  
+}
+function getFullLocation(userInfo){
+  const {location:{city, state, country}} = userInfo;
+  return <div>{`City: ${city}`} <br/>{`State: ${state}`} <br/>{`Country: ${country}`}</div>
+}
+
 
 function App() {
+
+  // url: https://randomuser.me/api
+
+  const [randomUserJSON, setRandomUserJSON ] = useState('')
+  const [userData, setUserData] = useState([])
+  useEffect(()=>{
+    getResponce().then((randomData)=>{
+      setRandomUserJSON(JSON.stringify(randomData,null, 2));
+      setUserData(randomData.results)
+    })
+
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='randomUsers'> {
+          userData.map((randomUsers, index)=>{
+           return <div key={index}>
+            <img src={randomUsers.picture.large} />
+            <p>{getFullUserName(randomUsers)}</p>
+            <p>{getFullLocation(randomUsers)}</p>
+            </div>
+          })
+        }
+
+    </div>
+      
+      <pre>
+        {randomUserJSON}
+      </pre>
+      
     </div>
   );
 }
 
 export default App;
+
